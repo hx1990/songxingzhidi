ccd56c028cae8e85dab3cadb11a3cb9d41565d74
hx1990 <742235410@qq.com>
2018-06-03 20:01:57 +0800

    2018/06/03修改

--- client/pages/expressFees/expressFees.js
+++ client/pages/expressFees/expressFees.js
@@ -1,5 +1,7 @@
 import {citylist} from "../../utils/util"
 const log=console.log.bind(console)
+let allCity = []
+let noeCity=[]
 Page({
   data: {
     array:[],
@@ -9,7 +11,12 @@ Page({
     ],
     companyIndex:0,
     loopLine: ['第一环线', '第二环线', '第三环线', '第四环线', '第五环线'],
-    loopIndex:0
+    loopIndex:0,
+    loopNoe:[],
+    fristWeight:0,
+    nextWeight:0,
+    fliePrice:0,
+    showList:[]
   },
   companyChange(e){
     this.setData({
@@ -26,21 +33,82 @@ Page({
       let arr=[]
       citylist.forEach((key,index)=>{
         arr.push(key.name)
+        allCity.push(key.name)
       })
       this.setData({
         array:arr
       })
   },
-
-  bindPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+  //选择城市
+  selectCity(e){
+    let index = e.target.dataset.index
+    noeCity.push(this.data.array[index])
+    let tempCity = this.data.array
+    tempCity.splice(index, 1)
+    this.setData({
+      loopNoe:noeCity,
+      array:tempCity
+    })
+  },
+  //取消选择
+  deleteCity(e){
+    let index = e.target.dataset.index
+    let tempCity = this.data.array
+    tempCity.push(this.data.loopNoe[index])
+    noeCity.splice(index, 1)
+    this.setData({
+      loopNoe: noeCity,
+      array: tempCity
+    })
+  },
+  bindPickerChange(e) {
     this.setData({
       index: e.detail.value
     })
   },
- 
+  loopChange(e){
+    this.setData({
+      loopIndex: e.detail.value
+    })
+  },
+  fristWeight(e){
+    this.setData({
+      fristWeight: Number(e.detail.value)
+    })
+  },
+  nextWeight(e) {
+    this.setData({
+      nextWeight: Number(e.detail.value)
+    })
+   
+  },
+  filePrice(e) {
+    this.setData({
+      filePrice: Number(e.detail.value)
+    })
+  },
+  submit(){
+    let arr = []
+    log(this.data.loopIndex)
+    if (this.data.loopIndex==0){
+      this.data.loopNoe.forEach((key,index)=>{
+        let json={}
+        json.loop='1环'
+        json.city=key
+        json.filePrice=this.data.filePrice
+        json.fristWeight = this.data.fristWeight
+        json.nextWeight=this.data.nextWeight
+        json.action='修改'
+        arr.push(json)
+      })
+    }
+    this.setData({
+      showList:arr
+    })
+    
+  },
   onReady: function () {
-  
+
   },
 
  
